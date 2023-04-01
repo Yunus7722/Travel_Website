555d01e302630d495283bfd05c34921df3254549
Yunus7722 <mohammedyunus7722@gmail.com>
2023-04-01 18:19:51 +0530

    First Commit for Travel Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+import Home from './Routes/Home';
+import Service from './Routes/Service';
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+     <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/service' element={<Service/>} />
+      </Routes>
+     </Router>
+  )
 }
 
-export default App;
+export default App
